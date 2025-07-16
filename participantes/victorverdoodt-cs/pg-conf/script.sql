663894abc5dec100a53a02902ac829bc3e78ee52
victor.verdoodt <victor.verdoodt@becareapp.io>
2025-07-16 16:19:20 -0300

    Remove Result
    Change SQL

--- participantes/victorverdoodt-cs/pg-conf/script.sql
+++ participantes/victorverdoodt-cs/pg-conf/script.sql
@@ -1,9 +1,9 @@
 CREATE UNLOGGED TABLE "Transactions" (
     "Id" UUID PRIMARY KEY,
-    "Amout" DECIMAL NOT NULL,
+    "Amount" DECIMAL NOT NULL,
     "requestedAt" TIMESTAMP NOT NULL,
     "Gateway" SMALLINT NOT NULL,
     "Status" SMALLINT NOT NULL DEFAULT 0
 );
 
-CREATE INDEX idx_transactions_covering_stats ON "Transactions" ("requestedAt", "Gateway") INCLUDE ("Amout");
+CREATE INDEX IX_Transactions_Summary_Covering ON "Transactions" ("Status", "Gateway", "requestedAt") INCLUDE ("Amount");
