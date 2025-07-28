ead2ef68454adee94262a8fb6a1febfc53b3c97c
Ruan Pablo <ruanpablocutrimramon@gmail.com>
2025-07-28 14:30:17 -0300

    refactor: remove unused Redis wait scripts

--- participantes/RuanPabloCR-Rruim/start-worker.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-until redis-cli -h redis -p 6379 ping | grep PONG; do
-  echo "Aguardando Redis responder PING..."
-  sleep 1
-done
-
-echo "Redis conectado! Iniciando worker.R..."
-exec R --no-restore --no-save -e "source('worker.R')"
