eb309c9b682041e557539ad58405c77b6210f946
Michel Oliveira <michel.nyx@hotmail.com>
2025-07-28 12:11:05 -0300

    fix: remove unwanted sql file

--- participantes/micheloliveira-com-dotnet-csharp-redis/init.sql
+++
@@ -1,8 +0,0 @@
-CREATE UNLOGGED TABLE payments (
-                          correlation_id UUID PRIMARY KEY,
-                          amount DECIMAL NOT NULL,
-                          processor TEXT NOT NULL,
-                          requested_at TIMESTAMPTZ NOT NULL
-);
-
-create index CONCURRENTLY idx_requested_at_processor ON payments(requested_at, processor);
