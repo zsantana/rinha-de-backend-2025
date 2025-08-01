6bf900a2589ee0c8a2e7d98160deba8e7d637b1c
Wellington Pacheco <wellingtonpacheco14@gmail.com>
2025-08-01 18:00:19 -0300

    refactor fallback logic

--- participantes/wellington-pacheco-fine-tunning/init.sql
+++ participantes/wellington-pacheco-fine-tunning/init.sql
@@ -1,3 +1,16 @@
+CREATE UNLOGGED TABLE IF NOT EXISTS payment_services (
+    name TEXT PRIMARY KEY,
+    failing BOOLEAN,
+    delay INTEGER
+);
+
+DELETE FROM payment_services;
+
+INSERT INTO payment_services (name, failing, delay)
+VALUES
+    ('default', FALSE, 0),
+    ('fallback', FALSE, 0);
+
 CREATE UNLOGGED TABLE IF NOT EXISTS payments (
     correlation_id UUID PRIMARY KEY,
     amount DECIMAL,
