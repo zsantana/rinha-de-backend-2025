69e493704895ba6d5490ddac311320f90463cb41
Leandro Alves <leandroprogramadoruxdesign@gmail.com>
2025-08-07 20:53:29 -0300

    v1.0 haskell

---
+++ participantes/leandroalves-haskell/libpq/install-libpq.sh
@@ -0,0 +1,13 @@
+# Keys
+apt -y install curl ca-certificates
+install -d /usr/share/postgresql-common/pgdg
+curl -o /usr/share/postgresql-common/pgdg/apt.postgresql.org.asc --fail https://www.postgresql.org/media/keys/ACCC4CF8.asc
+
+# Add apt repository
+. /etc/os-release
+sh -c "echo 'deb [signed-by=/usr/share/postgresql-common/pgdg/apt.postgresql.org.asc] https://apt.postgresql.org/pub/repos/apt $VERSION_CODENAME-pgdg main' > /etc/apt/sources.list.d/pgdg.list"
+
+apt update
+
+apt -y install libpq-dev
+
