3d2b1f1e5fdb65f3a41302aaef695ede23ffd859
hfurlan <hildebrando.furlan@gmail.com>
2025-08-10 19:50:02 -0300

    Correção no endpoint /payments-summary

--- participantes/hfurlan-java-v1/sql/ddl.sql
+++ participantes/hfurlan-java-v1/sql/ddl.sql
@@ -1,8 +1,11 @@
 create unlogged table payments (
     correlation_id char(36) unique,
     amount decimal not null,
-    payment_processor int not null
+    payment_processor int not null,
+    inserted_at timestamp not null
 );
+
+create index idx_payments_inserted_at on payments(inserted_at);
 
 create unlogged table payment_processor_priority (
     payment_processor int not null,
