ab69731464dec62625381952a19cddc92f99c50d
Daniel Nery <danielnso97@gmail.com>
2025-08-11 01:31:13 -0300

    Add submission

---
+++ participantes/dnery/README.md
@@ -0,0 +1,9 @@
+# Rinha de Backend 2025 - Submissão
+
+### Tecnologias
+
+- Linguagem: C# 10 (preview) + Dapper
+- Armazenamento: Postgres
+- Load Balancer: Nginx
+
+Repositório: https://github.com/d-nery/rinha-backend-2025
---
+++ participantes/dnery/init.sql
@@ -0,0 +1,8 @@
+CREATE UNLOGGED TABLE payments (
+    correlation_id UUID PRIMARY KEY,
+    amount DECIMAL NOT NULL,
+    processor VARCHAR(8) NOT NULL,
+    requested_at TIMESTAMP NOT NULL
+);
+
+CREATE INDEX CONCURRENTLY payments_requested_at ON payments (requested_at, processor);
