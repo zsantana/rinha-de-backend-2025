83542da52db1095fe7f62ee488a3c8d5203c5392
Danilo Santana <196120355+danilosantanaaaa@users.noreply.github.com>
2025-08-13 05:08:20 +0000

    Fazendo ajuste no docker compose, nginx e outros e utilizando binario nativo.

--- participantes/danilosantana-dotnet/init.sql
+++ participantes/danilosantana-dotnet/init.sql
@@ -3,7 +3,7 @@ TABLE payments (
     correlationId UUID PRIMARY KEY,
     amount DECIMAL NOT NULL,
     requested_at TIMESTAMP NOT NULL,
-    gateway smallint not null
+    gateway text not null
 );
 
 CREATE INDEX payments_requested_at ON payments (requested_at);
