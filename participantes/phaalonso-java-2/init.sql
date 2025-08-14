cfac62347e15e59b41c4e2b6da0b21d3ee56d665
Pedro Henrique de Almeida Alonso <phaalonso@gmail.com>
2025-08-14 01:05:25 -0300

    update script

--- participantes/phaalonso-java-2/init.sql
+++ participantes/phaalonso-java-2/init.sql
@@ -1,7 +1,8 @@
-create table payment (
+create unlogged table payment (
     correlationId uuid not null,
     amount decimal(15,2) not null,
     requestedAt timestamp not null
 );
 
-create index payment_requested_at on payment (requestedAt);
+create index idx_payment_requested_at
+    on payment (requestedAt) include (amount)
