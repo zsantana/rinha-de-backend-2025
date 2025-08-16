a5ef3e7cfcc929fc9f5afd422089ec187fd07e9c
igor lucas assuncao <igorlucas1217@gmail.com>
2025-08-15 23:02:05 -0300

    feat!: Alterando para redis; remove utilização de channels

--- participantes/igor-lucas-csharp/init.sql
+++ participantes/igor-lucas-csharp/init.sql
@@ -1,8 +1,8 @@
-CREATE TABLE payments (
+CREATE UNLOGGED TABLE payments (
                           CorrelationId UUID PRIMARY KEY,
                           Amount DECIMAL NOT NULL,
                           IsDefault BOOLEAN NOT NULL,
                           RequestedAt TIMESTAMP NOT NULL
 );
 
-create index CONCURRENTLY idx_requested_at_processor ON payments(RequestedAt, RequestedAt);
+create index CONCURRENTLY idx_requested_at_processor ON payments(RequestedAt, IsDefault);
